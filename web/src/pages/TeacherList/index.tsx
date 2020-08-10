c1ec8191aa85d4ebdef0aa2899a80cd2e7b1e072
Thiago Dornelles <thiago.fdornelles@gmail.com>
2020-08-09 21:01:23 -0300

    input component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 import './styles.css'
 
@@ -10,28 +11,15 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
+          <Input name="subject" label="Matéria" />
 
-          <div className="input-block">
-            <label htmlFor="subject">Dia da semana</label>
-            <input type="text" id="week_day"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="subject">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+          <Input name="week_day" label="Dia da semana" />
+          
+          <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
         <TeacherItem />
         <TeacherItem />
         <TeacherItem />
